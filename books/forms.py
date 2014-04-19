be0b112e2eaa7a1918caa1101c4a85245e8630ce
zhongwuzw <zhongwuzw@qq.com>
2014-04-19 10:28:12 +0800

    New Add

---
+++ books/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+class Contactform(forms.Form):
+    subject = forms.CharField(max_length=100)
+    email = forms.EmailField(required=False,label='Your e-mail  address')
+    message = forms.CharField(widget=forms.Textarea)
